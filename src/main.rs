813be619264bb5855e5df13d6ca5ce515c21b243
Nicolas BAUW <nicolas.bauw@leroymerlin.fr>
2019-09-18 15:19:17 +0200

    Function in an implementation + file validity test

--- src/main.rs
+++ src/main.rs
@@ -19,26 +19,26 @@ struct Tzfile {
 }
 
 fn main() {
-    read_tzdata();
+    Tzfile::read();
 }
 
-fn read_tzdata() {
-    let mut f = File::open("/Users/nicolasb/Dev/tz/usr/share/zoneinfo/America/Phoenix").unwrap();
-    let mut buffer = Vec::new();
-    // read the whole file
-    f.read_to_end(&mut buffer).unwrap();
-    //let magic=&buffer[0..=3];
-    //println!("{:?}", from_utf8(magic));
-    let header = Tzfile {
-        magic: BE::read_u32(&buffer[0x00..=0x03]),
-        version: buffer[4],
-        tzh_ttisgmtcnt: BE::read_i32(&buffer[0x14..=0x17]),
-        tzh_ttisstdcnt: BE::read_i32(&buffer[0x18..=0x1B]),
-        tzh_leapcnt: BE::read_i32(&buffer[0x1C..=0x1F]),
-        tzh_timecnt: BE::read_i32(&buffer[0x20..=0x23]),
-        tzh_typecnt: BE::read_i32(&buffer[0x24..=0x27]),
-        tzh_charcnt: BE::read_i32(&buffer[0x28..=0x2b]),
-    };
-    println!("{:?}", header.magic == MAGIC);
-    println!("{:?}", header);
+impl Tzfile {
+    fn read() {
+        let mut f = File::open("/Users/nicolasb/Dev/tz/usr/share/zoneinfo/America/Phoenix").unwrap();
+        let mut buffer = Vec::new();
+        // read the whole file
+        f.read_to_end(&mut buffer).unwrap();
+        let header = Tzfile {
+            magic: BE::read_u32(&buffer[0x00..=0x03]),
+            version: buffer[4],
+            tzh_ttisgmtcnt: BE::read_i32(&buffer[0x14..=0x17]),
+            tzh_ttisstdcnt: BE::read_i32(&buffer[0x18..=0x1B]),
+            tzh_leapcnt: BE::read_i32(&buffer[0x1C..=0x1F]),
+            tzh_timecnt: BE::read_i32(&buffer[0x20..=0x23]),
+            tzh_typecnt: BE::read_i32(&buffer[0x24..=0x27]),
+            tzh_charcnt: BE::read_i32(&buffer[0x28..=0x2b]),
+            };
+        println!("Valid TZfile : {}", (header.magic == MAGIC));
+        println!("{:?}", header);
+    }
 }
