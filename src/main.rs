4a4ebdb0543a1f479664b3567e3a526d41ec766c
Nicolas BAUW <nicolas.bauw@leroymerlin.fr>
2019-09-19 16:25:08 +0200

    Parses timezone names. Changed types u32 -> usize.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::env;
 use std::path::Path;
-//use std::str::from_utf8;
+use std::str::from_utf8;
 use byteorder::{ByteOrder, BE};
 
 fn main() {
@@ -19,18 +19,18 @@ fn main() {
 // TZif magic four bytes
 static MAGIC: u32 = 0x545A6966;
 // End of first (V1) header
-static V1_HEADER_END: i32 = 0x2C;
+static V1_HEADER_END: usize = 0x2C;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 struct Tzfile {
     magic: u32,
     version: u8,
-    tzh_ttisgmtcnt: i32,
-    tzh_ttisstdcnt: i32,
-    tzh_leapcnt: i32,
-    tzh_timecnt: i32,
-    tzh_typecnt: i32,
-    tzh_charcnt: i32,
+    tzh_ttisgmtcnt: usize,
+    tzh_ttisstdcnt: usize,
+    tzh_leapcnt: usize,
+    tzh_timecnt: usize,
+    tzh_typecnt: usize,
+    tzh_charcnt: usize,
 }
 
 impl Tzfile {
@@ -38,35 +38,38 @@ impl Tzfile {
         Tzfile {
             magic: BE::read_u32(&buffer[0x00..=0x03]),
             version: buffer[4],
-            tzh_ttisgmtcnt: BE::read_i32(&buffer[0x14..=0x17]),
-            tzh_ttisstdcnt: BE::read_i32(&buffer[0x18..=0x1B]),
-            tzh_leapcnt: BE::read_i32(&buffer[0x1C..=0x1F]),
-            tzh_timecnt: BE::read_i32(&buffer[0x20..=0x23]),
-            tzh_typecnt: BE::read_i32(&buffer[0x24..=0x27]),
-            tzh_charcnt: BE::read_i32(&buffer[0x28..=0x2b]),
+            tzh_ttisgmtcnt: BE::read_i32(&buffer[0x14..=0x17]) as usize,
+            tzh_ttisstdcnt: BE::read_i32(&buffer[0x18..=0x1B]) as usize,
+            tzh_leapcnt: BE::read_i32(&buffer[0x1C..=0x1F]) as usize,
+            tzh_timecnt: BE::read_i32(&buffer[0x20..=0x23]) as usize,
+            tzh_typecnt: BE::read_i32(&buffer[0x24..=0x27]) as usize,
+            tzh_charcnt: BE::read_i32(&buffer[0x28..=0x2b]) as usize,
         }
     }
 
-    fn parse(self, buffer: &[u8]) {
+    fn parse(&self, buffer: &[u8]) {
         // Calculates fields lengths and indexes (Version 1 format)
-        let tzh_timecnt_len = &self.tzh_timecnt*5;
-        let tzh_typecnt_len = &self.tzh_typecnt*6;
-        let tzh_leapcnt_len = &self.tzh_leapcnt*4;
-        let tzh_charcnt_len = &self.tzh_charcnt;
-        let tzh_timecnt_end = V1_HEADER_END + tzh_timecnt_len;
-        let tzh_typecnt_end = tzh_timecnt_end + tzh_typecnt_len;
-        let tzh_leapcnt_end = tzh_typecnt_end + tzh_leapcnt_len;
-        let tzh_charcnt_end = tzh_leapcnt_end + tzh_charcnt_len;
+        let tzh_timecnt_len: usize = self.tzh_timecnt*5;
+        let tzh_typecnt_len: usize = self.tzh_typecnt*6;
+        let tzh_leapcnt_len: usize = self.tzh_leapcnt*4;
+        let tzh_charcnt_len: usize = self.tzh_charcnt;
+        let tzh_timecnt_end: usize = V1_HEADER_END + tzh_timecnt_len;
+        let tzh_typecnt_end: usize = tzh_timecnt_end + tzh_typecnt_len;
+        let tzh_leapcnt_end: usize = tzh_typecnt_end + tzh_leapcnt_len;
+        let tzh_charcnt_end: usize = tzh_leapcnt_end + tzh_charcnt_len;
         println!("tzh_timecnt_len : {:x?}       tzh_timecnt_end : {:x?}", tzh_timecnt_len, tzh_timecnt_end);
         println!("tzh_typecnt_len : {:x?}       tzh_typecnt_end : {:x?}", tzh_typecnt_len, tzh_typecnt_end);
         println!("tzh_leapcnt_len : {:x?}       tzh_leapcnt_end : {:x?}", tzh_leapcnt_len, tzh_leapcnt_end);
         println!("tzh_charcnt_len : {:x?}       tzh_charcnt_end : {:x?}", tzh_charcnt_len, tzh_charcnt_end);
+
+        let names = from_utf8(&buffer[tzh_leapcnt_end..tzh_charcnt_end]).unwrap();
+        println!("Timezone names : {:?}", names);
     }
 
     fn read(tz: &str) -> Vec<u8> {
-    let mut tz_files_location: String = String::from("/Users/nicolasb/Dev/tz/usr/share/zoneinfo/");
-    tz_files_location.push_str(tz);
-    let path = Path::new(&tz_files_location);
+    let mut tz_files_root: String = String::from("/Users/nicolasb/Dev/tz/usr/share/zoneinfo/");
+    tz_files_root.push_str(tz);
+    let path = Path::new(&tz_files_root);
     let mut f = File::open(path).unwrap();
     let mut buffer = Vec::new();
     // read the whole file
