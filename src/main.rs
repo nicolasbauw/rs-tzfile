30d3c591bad3f9fb6ca8c7e91cfc0eadbe5e71be
Nicolas BAUW <nbauw@hotmail.com>
2019-09-25 12:50:17 +0200

    Moved some code outside of main function

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,20 @@ use rstzfile::*;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let requested_timezone = &args[1];
+    parse(&requested_timezone);
+}
 
+fn parse(requested_timezone: &str) {
     let buffer = match Tzfile::read(&requested_timezone) {
         Ok(b) => b,
         Err(e) => { println!("{}",e) ; return }
     };
     let header = Tzfile::parse_header(&buffer);
 
-    match header {
-        Ok(h) => println!("{:?}", h.parse(&buffer)),
+    let timezone = match header {
+        Ok(h) => h.parse(&buffer),
         Err(e) => { println!("{}",e) ; return }
-    }
+    };
+
+    println!("{:?}", timezone);
 }
