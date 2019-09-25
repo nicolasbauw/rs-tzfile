34cdf809df26df84bc6f00e3c8ff11c0bf6eac92
Nicolas BAUW <nbauw@hotmail.com>
2019-09-25 18:31:36 +0200

    Computes timechanges (or latest) for given year

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,44 @@
 extern crate rstzfile;
-use std::env;
+use std::{env, convert::TryInto};
 use rstzfile::*;
+use chrono::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let requested_timezone = &args[1];
-    parse(&requested_timezone);
+    let year = &args[2];
+    parse(&requested_timezone, year.parse().unwrap());
 }
 
-fn parse(requested_timezone: &str) {
+fn parse(requested_timezone: &str, year: i32) {
+    // Opens TZfile
     let buffer = match Tzfile::read(&requested_timezone) {
         Ok(b) => b,
         Err(e) => { println!("{}",e) ; return }
     };
+
+    // Parses TZfile header
     let header = Tzfile::parse_header(&buffer);
 
+    // Parses file content
     let timezone = match header {
         Ok(h) => h.parse(&buffer),
         Err(e) => { println!("{}",e) ; return }
     };
 
     println!("{:?}", timezone);
+
+    let mut timechanges = Vec::new();
+    let mut nearest_timechange: i32 = 0;
+    let currentyearbeg = Utc.ymd(year, 1, 1).and_hms(0, 0, 0);
+    let currentyearend = Utc.ymd(year, 12, 31).and_hms(0, 0, 0);
+    
+    for t in 0..timezone.tzh_timecnt_data.len() {
+        // Get timechanges for selected year
+        if timezone.tzh_timecnt_data[t] > currentyearbeg && timezone.tzh_timecnt_data[t] < currentyearend { timechanges.push(t); }
+        if timezone.tzh_timecnt_data[t] < currentyearbeg { nearest_timechange = t.try_into().unwrap(); };
+    }
+    
+    if timechanges.len() !=0 { println!("Time changes for specified year at index : {:?}", timechanges) }
+    else { println!("Latest time change for specified year at index : {:?}", nearest_timechange) };
 }
