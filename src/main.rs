a952af3c5002d7424da71921f7b37faebbed9966
Nicolas BAUW <nbauw@hotmail.com>
2019-09-26 18:53:08 +0200

    Exits without panic in case of error

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let requested_timezone = &args[1];
     let year = &args[2].parse().unwrap();
-    println!("{:?}", tz::get(&requested_timezone, *year).unwrap());
+    match tz::get(&requested_timezone, *year) {
+        Some(tz) => println!("{:?}", tz),
+        None => println!("Timezone not found")
+    };
 }
