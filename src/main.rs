588aeff2c9a14b58338ec87e3312e00198896ffa
Nicolas BAUW <nicolas.bauw@leroymerlin.fr>
2019-09-26 14:34:43 +0200

    export module renamed to tz

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-mod export;
+mod tz;
 use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let requested_timezone = &args[1];
     let year = &args[2];
-    export::export(&requested_timezone, year.parse().unwrap());
+    tz::export(&requested_timezone, year.parse().unwrap());
 }
