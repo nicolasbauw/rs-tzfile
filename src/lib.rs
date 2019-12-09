ebf3c7f6931dd2961be58bae6c41769cb4f915e7
Nicolas BAUW <nbauw@hotmail.com>
2019-12-09 19:51:50 +0100

    Can be used without chrono

--- src/lib.rs
+++ src/lib.rs
@@ -32,6 +32,7 @@
 //! The tests (cargo test) are written to match [2019c version of timezone database](https://www.iana.org/time-zones).
 
 use byteorder::{ByteOrder, BE};
+#[cfg(not(feature = "timestamp"))]
 use chrono::prelude::*;
 use dirs;
 use std::{env, error, fmt, fs::File, io::prelude::*, path::PathBuf, str::from_utf8};
@@ -82,6 +83,16 @@ impl From<TzError> for std::io::Error {
     }
 }
 
+#[cfg(feature = "timestamp")]
+#[derive(Debug)]
+pub struct Tz {
+    pub tzh_timecnt_data: Vec<i32>,
+    pub tzh_timecnt_indices: Vec<u8>,
+    pub tzh_typecnt: Vec<Ttinfo>,
+    pub tz_abbr: Vec<String>,
+}
+
+#[cfg(not(feature = "timestamp"))]
 #[derive(Debug)]
 pub struct Tz {
     pub tzh_timecnt_data: Vec<DateTime<Utc>>,
@@ -151,6 +162,14 @@ fn parse_data(header: Header, tz: &str) -> Result<Tz, TzError> {
     let tzh_charcnt_end: usize = tzh_leapcnt_end + tzh_charcnt_len;
 
     // Extracting data fields
+    #[cfg(feature = "timestamp")]
+    let tzh_timecnt_data: Vec<i32> = buffer
+        [V1_HEADER_END..V1_HEADER_END + header.tzh_timecnt * 4]
+        .chunks_exact(4)
+        .map(|tt| BE::read_i32(tt))
+        .collect();
+
+    #[cfg(not(feature = "timestamp"))]
     let tzh_timecnt_data: Vec<DateTime<Utc>> = buffer
         [V1_HEADER_END..V1_HEADER_END + header.tzh_timecnt * 4]
         .chunks_exact(4)
