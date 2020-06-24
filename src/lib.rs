7033098bad7023033d5c47f954fcc78c18913153
Nicolas BAUW <nbauw@hotmail.com>
2020-06-24 22:40:04 +0200

    WIP no chrono, no ENV

--- src/lib.rs
+++ src/lib.rs
@@ -1,52 +1,29 @@
-//! This low-level library reads the system timezone information files provided by IANA and returns a Tz struct representing the TZfile
+//! This library reads the system timezone information files provided by IANA and returns a Tz struct representing the TZfile
 //! fields as described in the man page (<http://man7.org/linux/man-pages/man5/tzfile.5.html>).
-//! Parses V2 (64 bits) format version since 1.0.0, so be aware that the tzh_timecnt_data field is now a `Vec<i64>`.
 //!
 //! For higher level parsing, see [my high-level parsing library](https://crates.io/crates/tzparse).
 //! 
-//! To keep the low-level aspect of the library, since 0.5.0 chrono is an optional feature which is not enabled by default, so tzh_timecnt_data is now the raw `i64` timestamp.
-//! For libtzfile to return tzh_timecnt_data as `DateTime<Utc>`, you can add this in Cargo.toml:
-//! ```text
-//! [dependencies.libtzfile]
-//! version = "1.0"
-//! features = ["with-chrono"]
-//! ```
 //! Here is an example:
 //!```
 //! fn main() {
-//!     println!("{:?}", libtzfile::parse("America/Phoenix").unwrap());
+//!     println!("{:?}", libtzfile::parse("/usr/share/zoneinfo/America/Phoenix").unwrap());
 //! }
 //!```
 //!
-//! which outputs (with chrono enabled):
 //!```text
-//! Tz { tzh_timecnt_data: [1883-11-18T19:00:00Z, 1918-03-31T09:00:00Z, 1918-10-27T08:00:00Z,
-//! 1919-03-30T09:00:00Z, 1919-10-26T08:00:00Z, 1942-02-09T09:00:00Z, 1944-01-01T06:01:00Z,
-//! 1944-04-01T07:01:00Z, 1944-10-01T06:01:00Z, 1967-04-30T09:00:00Z, 1967-10-29T08:00:00Z],
+//! Tz { tzh_timecnt_data: [FFFFFFFF5E040CB0, FFFFFFFF9EA63A90, FFFFFFFF9FBB0780,
+//! FFFFFFFFA0861C90, FFFFFFFFA19AE980, FFFFFFFFCB890C90, FFFFFFFFCF17DF1C,
+//! FFFFFFFFCF8FE5AC, FFFFFFFFD0811A1C, FFFFFFFFFAF87510, FFFFFFFFFBE85800],
 //! tzh_timecnt_indices: [2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2],
 //! tzh_typecnt: [Ttinfo { tt_gmtoff: -26898, tt_isdst: 0, tt_abbrind: 0 },
 //! Ttinfo { tt_gmtoff: -21600, tt_isdst: 1, tt_abbrind: 1 }, Ttinfo { tt_gmtoff: -25200, tt_isdst: 0, tt_abbrind: 2 },
 //! Ttinfo { tt_gmtoff: -21600, tt_isdst: 1, tt_abbrind: 3 }], tz_abbr: ["LMT", "MDT", "MST", "MWT"] }
 //!```
 //! 
-//! By default, with chrono disabled, tzh_timecnt_data will be like:
-//! ```text
-//! tzh_timecnt_data: [FFFFFFFF5E040CB0, FFFFFFFF9EA63A90, FFFFFFFF9FBB0780,
-//! FFFFFFFFA0861C90, FFFFFFFFA19AE980, FFFFFFFFCB890C90, FFFFFFFFCF17DF1C,
-//! FFFFFFFFCF8FE5AC, FFFFFFFFD0811A1C, FFFFFFFFFAF87510, FFFFFFFFFBE85800]
-//! ```
-//! It uses system TZfiles (default location on Linux and Macos /usr/share/zoneinfo). On Windows, default expected location is HOME/.zoneinfo. You can override the TZfiles default location with the TZFILES_DIR environment variable. Example for Windows:
-//!
-//! $env:TZFILES_DIR="C:\Users\nbauw\Dev\rs-tzfile\zoneinfo\"; cargo run
-//! 
-//! The tests (cargo test) are written to match [2019c version of timezone database](https://www.iana.org/time-zones).
+//! The tests (cargo test) are written to match 2019c version of timezone database.
 
 use byteorder::{ByteOrder, BE};
-#[cfg(feature = "with-chrono")]
-use chrono::prelude::*;
-#[cfg(windows)]
-use dirs;
-use std::{env, error, fmt, fs::File, io::prelude::*, path::PathBuf, str::from_utf8};
+use std::{error, fmt, fs::File, io::prelude::*, str::from_utf8};
 
 // TZif magic four bytes
 static MAGIC: u32 = 0x545A6966;
@@ -112,19 +89,9 @@ impl From<TzError> for std::io::Error {
     }
 }
 
-#[cfg(not(feature = "with-chrono"))]
 #[derive(Debug)]
 pub struct Tz {
     pub tzh_timecnt_data: Vec<i64>,
-    pub tzh_timecnt_indices: Vec<u8>,
-    pub tzh_typecnt: Vec<Ttinfo>,
-    pub tz_abbr: Vec<String>,
-}
-
-#[cfg(feature = "with-chrono")]
-#[derive(Debug)]
-pub struct Tz {
-    pub tzh_timecnt_data: Vec<DateTime<Utc>>,
     pub tzh_timecnt_indices: Vec<u8>,
     pub tzh_typecnt: Vec<Ttinfo>,
     pub tz_abbr: Vec<String>,
@@ -203,18 +170,10 @@ fn parse_data(buffer: &Vec<u8>, header: Header) -> Result<Tz, TzError> {
     let tzh_charcnt_end: usize = tzh_leapcnt_end + tzh_charcnt_len;
 
     // Extracting data fields
-    #[cfg(not(feature = "with-chrono"))]
     let tzh_timecnt_data: Vec<i64> = buffer
         [HEADER_LEN + header.v2_header_start..HEADER_LEN + header.v2_header_start + header.tzh_timecnt * 8]
         .chunks_exact(8)
         .map(|tt| BE::read_i64(tt))
-        .collect();
-
-    #[cfg(feature = "with-chrono")]
-    let tzh_timecnt_data: Vec<DateTime<Utc>> = buffer
-        [HEADER_LEN + header.v2_header_start..HEADER_LEN + header.v2_header_start + header.tzh_timecnt * 8]
-        .chunks_exact(8)
-        .map(|tt| Utc.timestamp(BE::read_i64(tt).into(), 0))
         .collect();
 
     let tzh_timecnt_indices: &[u8] =
@@ -252,32 +211,7 @@ fn parse_data(buffer: &Vec<u8>, header: Header) -> Result<Tz, TzError> {
 }
 
 fn read(tz: &str) -> Result<Vec<u8>, std::io::Error> {
-
-    // Default TZ files expected location (windows) is HOME/.zoneinfo, can be overridden by ENV
-    #[cfg(windows)]
-    let mut tz_files_root = match env::var_os("TZFILES_DIR") {
-        Some(tz_dir) => {
-            let mut d = PathBuf::new();
-            d.push(tz_dir);
-            d
-            },
-        None => {
-            let mut d = dirs::home_dir().unwrap_or(PathBuf::from("C:\\Users"));
-            d.push(".zoneinfo");
-            d
-        }
-    };
-
-    // Default TZ files expected location (POSIX) is /usr/share/zoneinfo, can be overridden by ENV
-    #[cfg(not(windows))]
-    let mut tz_files_root = {
-        let mut d = PathBuf::new();
-        d.push(env::var("TZFILES_DIR").unwrap_or(format!("/usr/share/zoneinfo/")));
-        d
-    };
-
-    tz_files_root.push(tz);
-    let mut f = File::open(tz_files_root)?;
+    let mut f = File::open(tz)?;
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer)?;
     Ok(buffer)
@@ -304,7 +238,6 @@ mod tests {
         assert_eq!(parse("America/Phoenix").unwrap().tzh_timecnt_indices, amph);
     }
 
-    #[cfg(not(feature = "with-chrono"))]
     #[test]
     fn parse_timedata() {
         let amph: Vec<i64> = vec![
