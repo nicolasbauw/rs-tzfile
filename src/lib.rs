0ec190d68c98b928688b410cff528d99297018cf
Nicolas BAUW <nbauw@hotmail.com>
2024-03-24 14:58:58 +0100

    Tests moved in a separate file

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //! fields as described in the man page (<http://man7.org/linux/man-pages/man5/tzfile.5.html>).
 //!
 //! For higher level parsing, you can enable the **parse** or **json** features (merged from the former [tzparse](https://crates.io/crates/tzparse) library).
-//! 
+//!
 //! In this documentation's examples, *tzfile* is the TZfile's path, for instance "/usr/share/zoneinfo/Europe/Paris".
 //!
 //! Without any feature enabled, one available method : new(), which returns a Tz struct:
@@ -15,32 +15,32 @@
 //!```text
 //! Tz { tzh_timecnt_data: [-2717643600, -1633273200, -1615132800, -1601823600, -1583683200, -880210800, -820519140, -812653140, -796845540, -84380400, -68659200], tzh_timecnt_indices: [2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2], tzh_typecnt: [Ttinfo { tt_gmtoff: -26898, tt_isdst: 0, tt_abbrind: 0 }, Ttinfo { tt_gmtoff: -21600, tt_isdst: 1, tt_abbrind: 1 }, Ttinfo { tt_gmtoff: -25200, tt_isdst: 0, tt_abbrind: 2 }, Ttinfo { tt_gmtoff: -21600, tt_isdst: 1, tt_abbrind: 3 }], tz_abbr: ["LMT", "MDT", "MST", "MWT"] }
 //! ```
-//! 
+//!
 //! With the parse or json features enabled, you have access to additional methods.
 //! For instance, to display 2020 DST transitions in France, you can use the transition_times method:
-//! 
+//!
 //! ```rust
 //! # let tzfile = if cfg!(windows) { "c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris" } else { "/usr/share/zoneinfo/Europe/Paris" };
 //! use libtzfile::Tz;
 //! println!("{:?}", Tz::new(tzfile).unwrap().transition_times(Some(2020)).unwrap());
 //! ```
-//! 
+//!
 //! ```text
 //! [TransitionTime { time: 2020-03-29T01:00:00Z, utc_offset: 7200, isdst: true, abbreviation: "CEST" }, TransitionTime { time: 2020-10-25T01:00:00Z, utc_offset: 3600, isdst: false, abbreviation: "CET" }]
 //! ```
-//! 
+//!
 //! If you want more complete information about the timezone, you can use the zoneinfo method, which returns a more complete structure:
-//! 
+//!
 //! ```rust
 //! # let tzfile = if cfg!(windows) { "c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris" } else { "/usr/share/zoneinfo/Europe/Paris" };
 //! use libtzfile::Tz;
 //! println!("{:?}", Tz::new(tzfile).unwrap().zoneinfo().unwrap());
 //!```
-//! 
+//!
 //! ```text
 //! Tzinfo { timezone: "Europe/Paris", utc_datetime: 2020-09-05T16:41:44.279502100Z, datetime: 2020-09-05T18:41:44.279502100+02:00, dst_from: Some(2020-03-29T01:00:00Z), dst_until: Some(2020-10-25T01:00:00Z), dst_period: true, raw_offset: 3600, dst_offset: 7200, utc_offset: +02:00, abbreviation: "CEST", week_number: 36 }
 //! ```
-//! 
+//!
 //! This more complete structure implements the Serialize trait and can also be transformed to a json string via a method of the json feature (which includes methods from the parse feature):
 //!```rust
 //! # let tzfile = if cfg!(windows) { "c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris" } else { "/usr/share/zoneinfo/Europe/Paris" };
@@ -51,21 +51,24 @@
 //! println!("{}", tz);
 //! # Ok::<(), TzError>(())
 //!```
-//! 
+//!
 //!```text
 //! {"timezone":"Europe/Paris","utc_datetime":"2020-09-05T18:04:50.546668500Z","datetime":"2020-09-05T20:04:50.546668500+02:00","dst_from":"2020-03-29T01:00:00Z","dst_until":"2020-10-25T01:00:00Z","dst_period":true,"raw_offset":3600,"dst_offset":7200,"utc_offset":"+02:00","abbreviation":"CEST","week_number":36}
 //!```
-//! 
+//!
 //! This feature is used in my [world time API](https://crates.io/crates/world-time-api).
-//! 
+//!
 //! The tests (cargo test --features json) are working with the [2022a timezone database](https://data.iana.org/time-zones/tz-link.html) (MacOS 12.4).
 
 use byteorder::{ByteOrder, BE};
 #[cfg(any(feature = "parse", feature = "json"))]
-use chrono::{DateTime, TimeZone, Utc, FixedOffset};
-use std::{error, fmt, fs::File, io::prelude::*, str::from_utf8};
+use chrono::{DateTime, FixedOffset, TimeZone, Utc};
 #[cfg(feature = "json")]
 use serde::Serialize;
+use std::{error, fmt, fs::File, io::prelude::*, str::from_utf8};
+
+#[cfg(test)]
+mod tests;
 
 #[cfg(feature = "json")]
 mod offset_serializer {
@@ -104,7 +107,7 @@ pub enum TzError {
     // Empty String
     EmptyString,
     // Json conversion error
-    JsonError
+    JsonError,
 }
 
 impl fmt::Display for TzError {
@@ -118,7 +121,7 @@ impl fmt::Display for TzError {
             TzError::NoData => "No data matched the request",
             TzError::ParseError => "Parsing error",
             TzError::EmptyString => "Empty string",
-            TzError::JsonError => "Could not convert to json"
+            TzError::JsonError => "Could not convert to json",
         })
     }
 }
@@ -206,7 +209,7 @@ pub struct TransitionTime {
 
 /// Convenient and human-readable informations about a timezone (available with the parse or json features).
 /// With the json feature enabled, the Tzinfo struct implements the Serialize trait.
-/// 
+///
 /// Some explanations about the offset fields:
 /// - raw_offset : the "normal" offset to utc, in seconds
 /// - dst_offset : the offset to utc during daylight saving time, in seconds
@@ -269,7 +272,7 @@ pub struct Tzinfo {
 #[cfg(feature = "json")]
 impl Tzinfo {
     /// Transforms the Tzinfo struct to a JSON string
-    /// 
+    ///
     ///```rust
     /// # let tzfile = if cfg!(windows) { "c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris" } else { "/usr/share/zoneinfo/Europe/Paris" };
     /// use libtzfile::{Tz, TzError};
@@ -279,7 +282,7 @@ impl Tzinfo {
     /// println!("{}", tz);
     /// # Ok::<(), TzError>(())
     ///```
-    /// 
+    ///
     ///```text
     /// {"timezone":"Europe/Paris","utc_datetime":"2020-09-05T18:04:50.546668500Z","datetime":"2020-09-05T20:04:50.546668500+02:00","dst_from":"2020-03-29T01:00:00Z","dst_until":"2020-10-25T01:00:00Z","dst_period":true,"raw_offset":3600,"dst_offset":7200,"utc_offset":"+02:00","abbreviation":"CEST","week_number":36}
     ///```
@@ -296,7 +299,7 @@ impl Tz {
     /// use libtzfile::Tz;
     /// let tz = Tz::new(tzfile).unwrap();
     /// ```
-    /// 
+    ///
     ///```text
     /// Tz { tzh_timecnt_data: [-2717643600, -1633273200, -1615132800, -1601823600, -1583683200, -880210800, -820519140, -812653140, -796845540, -84380400, -68659200], tzh_timecnt_indices: [2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2], tzh_typecnt: [Ttinfo { tt_gmtoff: -26898, tt_isdst: 0, tt_abbrind: 0 }, Ttinfo { tt_gmtoff: -21600, tt_isdst: 1, tt_abbrind: 1 }, Ttinfo { tt_gmtoff: -25200, tt_isdst: 0, tt_abbrind: 2 }, Ttinfo { tt_gmtoff: -21600, tt_isdst: 1, tt_abbrind: 3 }], tz_abbr: ["LMT", "MDT", "MST", "MWT"] }
     /// ```
@@ -314,13 +317,13 @@ impl Tz {
     /// If year is Some(0), returns current year's transition times.
     /// If there's no transition time for selected year, returns the last occured transition time (zone's current parameters).
     /// If no year (None) is specified, returns all transition times recorded in the TZfile .
-    /// 
+    ///
     /// ```rust
     /// # let tzfile = if cfg!(windows) { "c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris" } else { "/usr/share/zoneinfo/Europe/Paris" };
     /// use libtzfile::Tz;
     /// println!("{:?}", Tz::new(tzfile).unwrap().transition_times(Some(2020)).unwrap());
     /// ```
-    /// 
+    ///
     /// ```text
     /// [TransitionTime { time: 2020-03-29T01:00:00Z, utc_offset: 7200, isdst: true, abbreviation: "CEST" }, TransitionTime { time: 2020-10-25T01:00:00Z, utc_offset: 3600, isdst: false, abbreviation: "CET" }]
     /// ```
@@ -330,7 +333,7 @@ impl Tz {
         // Fix for issue #3 "Calling zoneinfo on a file without transition times panics"
         // We return a NoData error if no transition times are recorded in the TZFile.
         if timezone.tzh_timecnt_data.len() == 0 {
-            return Err(TzError::NoData)
+            return Err(TzError::NoData);
         }
 
         // used to store transition time indices
@@ -418,7 +421,7 @@ impl Tz {
     /// use libtzfile::Tz;
     /// println!("{:?}", Tz::new(tzfile).unwrap().zoneinfo().unwrap());
     /// ```
-    /// 
+    ///
     /// ```text
     /// Tzinfo { timezone: "Europe/Paris", utc_datetime: 2020-09-05T16:41:44.279502100Z, datetime: 2020-09-05T18:41:44.279502100+02:00, dst_from: Some(2020-03-29T01:00:00Z), dst_until: Some(2020-10-25T01:00:00Z), dst_period: true, raw_offset: 3600, dst_offset: 7200, utc_offset: +02:00, abbreviation: "CEST", week_number: 36 }
     /// ```
@@ -426,7 +429,7 @@ impl Tz {
         let parsedtimechanges = match self.transition_times(Some(0)) {
             Ok(p) => p,
             Err(TzError::NoData) => Vec::new(),
-            Err(e) => { return Err(e) }
+            Err(e) => return Err(e),
         };
         let d = Utc::now();
         if parsedtimechanges.len() == 2 {
@@ -615,223 +618,5 @@ impl Tz {
         let mut buffer = Vec::new();
         f.read_to_end(&mut buffer)?;
         Ok(buffer)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::*;
-    #[cfg(target_os = "windows")]
-    static TIMEZONE: &str = "c:\\Users\\nbauw\\Dev\\zoneinfo\\America\\Phoenix";
-    #[cfg(target_family = "unix")]
-    static TIMEZONE: &str = "/usr/share/zoneinfo/America/Phoenix";
-    #[test]
-    fn read_file() {
-        assert_eq!(Tz::read(TIMEZONE).is_ok(), true);
-    }
-
-    #[test]
-    fn parse_hdr() {
-        let buf = Tz::read(TIMEZONE).unwrap();
-        let amph = Header {
-            tzh_ttisgmtcnt: 4,
-            tzh_ttisstdcnt: 4,
-            tzh_leapcnt: 0,
-            tzh_timecnt: 11,
-            tzh_typecnt: 4,
-            tzh_charcnt: 16,
-            v2_header_start: 130,
-        };
-        assert_eq!(Tz::parse_header(&buf).unwrap(), amph);
-    }
-
-    #[test]
-    fn parse_indices() {
-        let amph: [u8; 11] = [2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2];
-        assert_eq!(Tz::new(TIMEZONE).unwrap().tzh_timecnt_indices, amph);
-    }
-
-    #[test]
-    fn parse_timedata() {
-        let amph: Vec<i64> = vec![
-            -2717643600,
-            -1633273200,
-            -1615132800,
-            -1601823600,
-            -1583683200,
-            -880210800,
-            -820519140,
-            -812653140,
-            -796845540,
-            -84380400,
-            -68659200,
-        ];
-        assert_eq!(Tz::new(TIMEZONE).unwrap().tzh_timecnt_data, amph);
-    }
-
-    #[test]
-    fn parse_ttgmtoff() {
-        let amph: [isize; 4] = [-26898, -21600, -25200, -21600];
-        let c: Vec<isize> = Tz::new(TIMEZONE)
-            .unwrap()
-            .tzh_typecnt
-            .iter()
-            .map(|ttinfo| ttinfo.tt_gmtoff)
-            .collect();
-        assert_eq!(c, amph);
-    }
-
-    #[test]
-    fn parse_abbr() {
-        let abbr: Vec<String> = vec!["LMT", "MDT", "MST", "MWT"]
-            .iter()
-            .map(|x| x.to_string())
-            .collect();
-        assert_eq!(Tz::new(TIMEZONE).unwrap().tz_abbr, abbr);
-    }
-
-    #[test]
-    fn parse_abbr_amsterdam() {
-        #[cfg(target_os = "windows")]
-        let timezone = "c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Amsterdam";
-        #[cfg(target_family = "unix")]
-        let timezone = "/usr/share/zoneinfo/Europe/Amsterdam";
-        let abbr: Vec<String> = vec!["LMT", "NST", "AMT", "+0020", "+0120", "CET", "CEST"]
-            .iter()
-            .map(|x| x.to_string())
-            .collect();
-        assert_eq!(Tz::new(timezone).unwrap().tz_abbr, abbr);
-        dbg!(Tz::new(timezone).unwrap());
-    }
-
-    #[test]
-    fn zonename() {
-        let z = "America/Phoenix";
-        assert_eq!(Tz::new(TIMEZONE).unwrap().name, z);
-    }
-
-    // cargo test --features=parse
-    #[cfg(any(feature = "parse", feature = "json"))]
-    #[test]
-    fn partial_timechanges() {
-        let tt = vec![
-            TransitionTime {
-                time: Utc.ymd(2019, 3, 31).and_hms(1, 0, 0),
-                utc_offset: 7200,
-                isdst: true,
-                abbreviation: String::from("CEST"),
-            },
-            TransitionTime {
-                time: Utc.ymd(2019, 10, 27).and_hms(1, 0, 0),
-                utc_offset: 3600,
-                isdst: false,
-                abbreviation: String::from("CET"),
-            },
-        ];
-        #[cfg(target_family = "unix")]
-        let tz = Tz::new("/usr/share/zoneinfo/Europe/Paris").unwrap();
-        #[cfg(target_os = "windows")]
-        let tz = Tz::new("c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris").unwrap();
-        assert_eq!(tz.transition_times(Some(2019)).unwrap(), tt);
-    }
-
-    #[cfg(any(feature = "parse", feature = "json"))]
-    #[test]
-    fn total_timechanges() {
-        let tt = vec![
-            TransitionTime {
-                time: Utc.ymd(1883, 11, 18).and_hms(19, 0, 0),
-                utc_offset: -25200,
-                isdst: false,
-                abbreviation: String::from("MST"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1918, 03, 31).and_hms(9, 0, 0),
-                utc_offset: -21600,
-                isdst: true,
-                abbreviation: String::from("MDT"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1918, 10, 27).and_hms(8, 0, 0),
-                utc_offset: -25200,
-                isdst: false,
-                abbreviation: String::from("MST"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1919, 03, 30).and_hms(9, 0, 0),
-                utc_offset: -21600,
-                isdst: true,
-                abbreviation: String::from("MDT"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1919, 10, 26).and_hms(8, 0, 0),
-                utc_offset: -25200,
-                isdst: false,
-                abbreviation: String::from("MST"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1942, 02, 09).and_hms(9, 0, 0),
-                utc_offset: -21600,
-                isdst: true,
-                abbreviation: String::from("MWT"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1944, 01, 01).and_hms(6, 1, 0),
-                utc_offset: -25200,
-                isdst: false,
-                abbreviation: String::from("MST"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1944, 04, 01).and_hms(7, 1, 0),
-                utc_offset: -21600,
-                isdst: true,
-                abbreviation: String::from("MWT"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1944, 10, 01).and_hms(6, 1, 0),
-                utc_offset: -25200,
-                isdst: false,
-                abbreviation: String::from("MST"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1967, 04, 30).and_hms(9, 0, 0),
-                utc_offset: -21600,
-                isdst: true,
-                abbreviation: String::from("MDT"),
-            },
-            TransitionTime {
-                time: Utc.ymd(1967, 10, 29).and_hms(8, 0, 0),
-                utc_offset: -25200,
-                isdst: false,
-                abbreviation: String::from("MST"),
-            },
-        ];
-        let tz = Tz::new(TIMEZONE).unwrap();
-        assert_eq!(tz.transition_times(None).unwrap(), tt);
-    }
-
-    // cargo test --features=json
-    #[cfg(any(feature = "parse", feature = "json"))]
-    #[test]
-    fn zoneinfo() {
-        #[cfg(target_family = "unix")]
-        let tztest = Tz::new("/usr/share/zoneinfo/Europe/Paris").unwrap().zoneinfo().unwrap();
-        #[cfg(target_os = "windows")]
-        let tztest = (Tz::new("c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris").unwrap()).zoneinfo().unwrap();
-        assert_eq!(tztest.timezone, String::from("Europe/Paris"));
-        assert_eq!(tztest.raw_offset, 3600);
-        assert_eq!(tztest.dst_offset, 7200);
-    }
-
-    // cargo test --features=json
-    #[cfg(any(feature = "parse", feature = "json"))]
-    #[test]
-    fn emptytt() {
-        #[cfg(target_os = "windows")]
-        let timezone = "c:\\Users\\nbauw\\Dev\\zoneinfo\\EST";
-        #[cfg(target_family = "unix")]
-        let timezone = "/usr/share/zoneinfo/EST";
-        assert_eq!(Err(crate::TzError::NoData),
-        Tz::new(timezone).unwrap().transition_times(None));
     }
 }
---
+++ src/tests.rs
@@ -0,0 +1,221 @@
+use crate::*;
+#[cfg(target_os = "windows")]
+static TIMEZONE: &str = "c:\\Users\\nbauw\\Dev\\zoneinfo\\America\\Phoenix";
+#[cfg(target_family = "unix")]
+static TIMEZONE: &str = "/usr/share/zoneinfo/America/Phoenix";
+#[test]
+fn read_file() {
+    assert_eq!(Tz::read(TIMEZONE).is_ok(), true);
+}
+
+#[test]
+fn parse_hdr() {
+    let buf = Tz::read(TIMEZONE).unwrap();
+    let amph = Header {
+        tzh_ttisgmtcnt: 4,
+        tzh_ttisstdcnt: 4,
+        tzh_leapcnt: 0,
+        tzh_timecnt: 11,
+        tzh_typecnt: 4,
+        tzh_charcnt: 16,
+        v2_header_start: 130,
+    };
+    assert_eq!(Tz::parse_header(&buf).unwrap(), amph);
+}
+
+#[test]
+fn parse_indices() {
+    let amph: [u8; 11] = [2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2];
+    assert_eq!(Tz::new(TIMEZONE).unwrap().tzh_timecnt_indices, amph);
+}
+
+#[test]
+fn parse_timedata() {
+    let amph: Vec<i64> = vec![
+        -2717643600,
+        -1633273200,
+        -1615132800,
+        -1601823600,
+        -1583683200,
+        -880210800,
+        -820519140,
+        -812653140,
+        -796845540,
+        -84380400,
+        -68659200,
+    ];
+    assert_eq!(Tz::new(TIMEZONE).unwrap().tzh_timecnt_data, amph);
+}
+
+#[test]
+fn parse_ttgmtoff() {
+    let amph: [isize; 4] = [-26898, -21600, -25200, -21600];
+    let c: Vec<isize> = Tz::new(TIMEZONE)
+        .unwrap()
+        .tzh_typecnt
+        .iter()
+        .map(|ttinfo| ttinfo.tt_gmtoff)
+        .collect();
+    assert_eq!(c, amph);
+}
+
+#[test]
+fn parse_abbr() {
+    let abbr: Vec<String> = vec!["LMT", "MDT", "MST", "MWT"]
+        .iter()
+        .map(|x| x.to_string())
+        .collect();
+    assert_eq!(Tz::new(TIMEZONE).unwrap().tz_abbr, abbr);
+}
+
+#[test]
+fn parse_abbr_amsterdam() {
+    #[cfg(target_os = "windows")]
+    let timezone = "c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Amsterdam";
+    #[cfg(target_family = "unix")]
+    let timezone = "/usr/share/zoneinfo/Europe/Amsterdam";
+    let abbr: Vec<String> = vec!["LMT", "NST", "AMT", "+0020", "+0120", "CET", "CEST"]
+        .iter()
+        .map(|x| x.to_string())
+        .collect();
+    assert_eq!(Tz::new(timezone).unwrap().tz_abbr, abbr);
+    dbg!(Tz::new(timezone).unwrap());
+}
+
+#[test]
+fn zonename() {
+    let z = "America/Phoenix";
+    assert_eq!(Tz::new(TIMEZONE).unwrap().name, z);
+}
+
+// cargo test --features=parse
+#[cfg(any(feature = "parse", feature = "json"))]
+#[test]
+fn partial_timechanges() {
+    let tt = vec![
+        TransitionTime {
+            time: Utc.ymd(2019, 3, 31).and_hms(1, 0, 0),
+            utc_offset: 7200,
+            isdst: true,
+            abbreviation: String::from("CEST"),
+        },
+        TransitionTime {
+            time: Utc.ymd(2019, 10, 27).and_hms(1, 0, 0),
+            utc_offset: 3600,
+            isdst: false,
+            abbreviation: String::from("CET"),
+        },
+    ];
+    #[cfg(target_family = "unix")]
+    let tz = Tz::new("/usr/share/zoneinfo/Europe/Paris").unwrap();
+    #[cfg(target_os = "windows")]
+    let tz = Tz::new("c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris").unwrap();
+    assert_eq!(tz.transition_times(Some(2019)).unwrap(), tt);
+}
+
+#[cfg(any(feature = "parse", feature = "json"))]
+#[test]
+fn total_timechanges() {
+    let tt = vec![
+        TransitionTime {
+            time: Utc.ymd(1883, 11, 18).and_hms(19, 0, 0),
+            utc_offset: -25200,
+            isdst: false,
+            abbreviation: String::from("MST"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1918, 03, 31).and_hms(9, 0, 0),
+            utc_offset: -21600,
+            isdst: true,
+            abbreviation: String::from("MDT"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1918, 10, 27).and_hms(8, 0, 0),
+            utc_offset: -25200,
+            isdst: false,
+            abbreviation: String::from("MST"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1919, 03, 30).and_hms(9, 0, 0),
+            utc_offset: -21600,
+            isdst: true,
+            abbreviation: String::from("MDT"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1919, 10, 26).and_hms(8, 0, 0),
+            utc_offset: -25200,
+            isdst: false,
+            abbreviation: String::from("MST"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1942, 02, 09).and_hms(9, 0, 0),
+            utc_offset: -21600,
+            isdst: true,
+            abbreviation: String::from("MWT"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1944, 01, 01).and_hms(6, 1, 0),
+            utc_offset: -25200,
+            isdst: false,
+            abbreviation: String::from("MST"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1944, 04, 01).and_hms(7, 1, 0),
+            utc_offset: -21600,
+            isdst: true,
+            abbreviation: String::from("MWT"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1944, 10, 01).and_hms(6, 1, 0),
+            utc_offset: -25200,
+            isdst: false,
+            abbreviation: String::from("MST"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1967, 04, 30).and_hms(9, 0, 0),
+            utc_offset: -21600,
+            isdst: true,
+            abbreviation: String::from("MDT"),
+        },
+        TransitionTime {
+            time: Utc.ymd(1967, 10, 29).and_hms(8, 0, 0),
+            utc_offset: -25200,
+            isdst: false,
+            abbreviation: String::from("MST"),
+        },
+    ];
+    let tz = Tz::new(TIMEZONE).unwrap();
+    assert_eq!(tz.transition_times(None).unwrap(), tt);
+}
+
+// cargo test --features=json
+#[cfg(any(feature = "parse", feature = "json"))]
+#[test]
+fn zoneinfo() {
+    #[cfg(target_family = "unix")]
+    let tztest = Tz::new("/usr/share/zoneinfo/Europe/Paris")
+        .unwrap()
+        .zoneinfo()
+        .unwrap();
+    #[cfg(target_os = "windows")]
+    let tztest = (Tz::new("c:\\Users\\nbauw\\Dev\\zoneinfo\\Europe\\Paris").unwrap())
+        .zoneinfo()
+        .unwrap();
+    assert_eq!(tztest.timezone, String::from("Europe/Paris"));
+    assert_eq!(tztest.raw_offset, 3600);
+    assert_eq!(tztest.dst_offset, 7200);
+}
+
+// cargo test --features=json
+#[cfg(any(feature = "parse", feature = "json"))]
+#[test]
+fn emptytt() {
+    #[cfg(target_os = "windows")]
+    let timezone = "c:\\Users\\nbauw\\Dev\\zoneinfo\\EST";
+    #[cfg(target_family = "unix")]
+    let timezone = "/usr/share/zoneinfo/EST";
+    assert_eq!(
+        Err(crate::TzError::NoData),
+        Tz::new(timezone).unwrap().transition_times(None)
+    );
+}
