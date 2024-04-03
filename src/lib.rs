4ddeb73dd99617583f46039c0767e52335fa6878
Nicolas BAUW <nbauw@hotmail.com>
2024-04-03 07:01:23 +0200

    Tests passing with std feature

--- src/lib.rs
+++ src/lib.rs
@@ -60,17 +60,25 @@
 //!
 //! The tests (cargo test --features json) are working with the [2022a timezone database](https://data.iana.org/time-zones/tz-link.html) (MacOS 12.4).
 
+// Support using libtzfile without the standard library
 #![cfg_attr(not(feature = "std"), no_std)]
 
 #[cfg(feature = "std")]
 #[cfg(test)]
 mod tests;
+#[cfg(feature = "std")]
+extern crate std;
+#[cfg(feature = "std")]
+use std::{error, fmt, fs::File, io::Read, str::from_utf8};
 
+#[cfg(not(feature = "std"))]
 #[cfg(test)]
 mod tests_nostd;
-
+#[cfg(not(feature = "std"))]
 extern crate alloc;
+#[cfg(not(feature = "std"))]
 use alloc::{str::from_utf8, string::String, string::ToString, vec::Vec};
+
 use byteorder::{ByteOrder, BE};
 
 // TZif magic four bytes
@@ -98,6 +106,61 @@ pub enum TzError {
     JsonError,
 }
 
+#[cfg(feature = "std")]
+impl fmt::Display for TzError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str("TZfile error : ")?;
+        f.write_str(match self {
+            TzError::InvalidTimezone => "Invalid timezone",
+            TzError::InvalidMagic => "Invalid TZfile",
+            TzError::BadUtf8String => "Bad utf8 string",
+            TzError::UnsupportedFormat => "Only V2 format is supported",
+            TzError::NoData => "No data matched the request",
+            TzError::ParseError => "Parsing error",
+            TzError::EmptyString => "Empty string",
+            TzError::JsonError => "Could not convert to json",
+        })
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<std::io::Error> for TzError {
+    fn from(_e: std::io::Error) -> TzError {
+        TzError::InvalidTimezone
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<std::num::ParseIntError> for TzError {
+    fn from(_e: std::num::ParseIntError) -> TzError {
+        TzError::ParseError
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<std::str::Utf8Error> for TzError {
+    fn from(_e: std::str::Utf8Error) -> TzError {
+        TzError::BadUtf8String
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<TzError> for std::io::Error {
+    fn from(e: TzError) -> std::io::Error {
+        std::io::Error::new(std::io::ErrorKind::Other, e)
+    }
+}
+
+#[cfg(feature = "json")]
+impl From<serde_json::error::Error> for TzError {
+    fn from(_e: serde_json::error::Error) -> TzError {
+        TzError::JsonError
+    }
+}
+
+#[cfg(feature = "std")]
+impl error::Error for TzError {}
+
 /// This is the crate's primary structure, which contains the splitted TZfile fields and optional (via features) methods.
 #[derive(Debug)]
 pub struct Tz {
@@ -109,6 +172,8 @@ pub struct Tz {
     pub tzh_typecnt: Vec<Ttinfo>,
     /// abbreviations table
     pub tz_abbr: Vec<String>,
+    #[cfg(feature = "std")]
+    name: String,
 }
 
 /// This sub-structure of the Tz struct is part of the TZfile format specifications, and contains UTC offset, daylight saving time, abbreviation index.
@@ -131,11 +196,22 @@ struct Header {
 }
 
 impl Tz {
+    #[cfg(not(feature = "std"))]
     pub fn new(buf: Vec<u8>) -> Result<Tz, TzError> {
         // Parses TZfile header
         let header = Tz::parse_header(&buf)?;
         // Parses data
         Tz::parse_data(&buf, header)
+    }
+
+    #[cfg(feature = "std")]
+    pub fn new(tz: &str) -> Result<Tz, TzError> {
+        // Reads TZfile
+        let buf = Tz::read(tz)?;
+        // Parses TZfile header
+        let header = Tz::parse_header(&buf)?;
+        // Parses data
+        Tz::parse_data(&buf, header, tz)
     }
 
     fn parse_header(buffer: &Vec<u8>) -> Result<Header, TzError> {
@@ -171,6 +247,7 @@ impl Tz {
         })
     }
 
+    #[cfg(not(feature = "std"))]
     fn parse_data(buffer: &Vec<u8>, header: Header) -> Result<Tz, TzError> {
         // Calculates fields lengths and indexes (Version 2 format)
         let tzh_timecnt_len: usize = header.tzh_timecnt * 9;
@@ -222,6 +299,89 @@ impl Tz {
             tzh_timecnt_indices: tzh_timecnt_indices.to_vec(),
             tzh_typecnt,
             tz_abbr,
+        })
+    }
+
+    #[cfg(feature = "std")]
+    fn read(tz: &str) -> Result<Vec<u8>, std::io::Error> {
+        let mut f = File::open(tz)?;
+        let mut buffer = Vec::new();
+        f.read_to_end(&mut buffer)?;
+        Ok(buffer)
+    }
+
+    #[cfg(feature = "std")]
+    fn parse_data(buffer: &Vec<u8>, header: Header, filename: &str) -> Result<Tz, TzError> {
+        // Calculates fields lengths and indexes (Version 2 format)
+        let tzh_timecnt_len: usize = header.tzh_timecnt * 9;
+        let tzh_typecnt_len: usize = header.tzh_typecnt * 6;
+        let tzh_leapcnt_len: usize = header.tzh_leapcnt * 12;
+        let tzh_charcnt_len: usize = header.tzh_charcnt;
+        let tzh_timecnt_end: usize = HEADER_LEN + header.v2_header_start + tzh_timecnt_len;
+        let tzh_typecnt_end: usize = tzh_timecnt_end + tzh_typecnt_len;
+        let tzh_leapcnt_end: usize = tzh_typecnt_end + tzh_leapcnt_len;
+        let tzh_charcnt_end: usize = tzh_leapcnt_end + tzh_charcnt_len;
+
+        // Extracting data fields
+        let tzh_timecnt_data: Vec<i64> = buffer[HEADER_LEN + header.v2_header_start
+            ..HEADER_LEN + header.v2_header_start + header.tzh_timecnt * 8]
+            .chunks_exact(8)
+            .map(|tt| BE::read_i64(tt))
+            .collect();
+
+        let tzh_timecnt_indices: &[u8] =
+            &buffer[HEADER_LEN + header.v2_header_start + header.tzh_timecnt * 8..tzh_timecnt_end];
+
+        let abbrs = from_utf8(&buffer[tzh_leapcnt_end..tzh_charcnt_end])?;
+
+        let tzh_typecnt: Vec<Ttinfo> = buffer[tzh_timecnt_end..tzh_typecnt_end]
+            .chunks_exact(6)
+            .map(|tti| {
+                let offset = tti[5];
+                let index = abbrs
+                    .chars()
+                    .take(offset as usize)
+                    .filter(|x| *x == '\0')
+                    .count();
+                Ttinfo {
+                    tt_utoff: BE::read_i32(&tti[0..4]) as isize,
+                    tt_isdst: tti[4],
+                    tt_abbrind: index as u8,
+                }
+            })
+            .collect();
+
+        let mut tz_abbr: Vec<String> = abbrs.split("\u{0}").map(|st| st.to_string()).collect();
+        // Removes last empty char
+        if tz_abbr.pop().is_none() {
+            return Err(TzError::EmptyString);
+        };
+
+        // Generating zone name (ie. Europe/Paris) from requested file name
+        let mut timezone = String::new();
+        #[cfg(not(windows))]
+        let mut tz: Vec<&str> = filename.split("/").collect();
+        #[cfg(windows)]
+        let mut tz: Vec<&str> = filename.split("\\").collect();
+        // To prevent crash (case of requested directory separator unmatching OS separator)
+        if tz.len() < 3 {
+            return Err(TzError::InvalidTimezone);
+        }
+        for _ in 0..(tz.len()) - 2 {
+            tz.remove(0);
+        }
+        if tz[0] != "zoneinfo" {
+            timezone.push_str(tz[0]);
+            timezone.push_str("/");
+        }
+        timezone.push_str(tz[1]);
+
+        Ok(Tz {
+            tzh_timecnt_data: tzh_timecnt_data,
+            tzh_timecnt_indices: tzh_timecnt_indices.to_vec(),
+            tzh_typecnt: tzh_typecnt,
+            tz_abbr: tz_abbr,
+            name: timezone,
         })
     }
 }
