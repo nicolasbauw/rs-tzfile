784dbfdde3ff087dccb815447e3b2154ffda79fe
Nicolas BAUW <nbauw@hotmail.com>
2024-04-05 08:18:31 +0200

    Doc update

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,14 @@
-//! This library reads the system timezone information files provided by IANA and returns a Tz struct containing the TZfile
+//! This library reads the system timezone information files (TZ Files) provided by IANA.
+//!
+//! Without any feature enabled, the crate is ```no_std```, and the ```new(buf: Vec<u8>)``` method returns a Tz struct containing the TZfile
 //! fields as described in the man page (<http://man7.org/linux/man-pages/man5/tzfile.5.html>).
 //!
-//! For higher level parsing, you can enable the **parse** or **json** features (merged from the former [tzparse](https://crates.io/crates/tzparse) library).
+//! For error conversion, you can enable the ```std``` feature, and the ```new(tz: &str)``` method now requires the filename and opens this file for you.
+//!
+//! For higher level parsing, you can enable the **parse** or **json** features.
 //!
 //! In this documentation's examples, *tzfile* is the TZfile's path, for instance "/usr/share/zoneinfo/Europe/Paris".
 //!
-//! Without any feature enabled, the crate is ```no_std```, and there is only one method available : ```new(buf: Vec<u8>)```, which returns a Tz struct:
-//!
-//!```text
-//! extern crate std;
-//! use libtzfile::Tz;
-//! let buf = std::fs::read("/usr/share/zoneinfo/America/Phoenix").unwrap();
-//! let tz = Tz::new(buf).unwrap();
-//!```
-//! Note that by default, the crate is ```no_std``` and does not provide error conversion.
-//!
-//! With the std feature enabled, the method becomes ```new(tz: &str)``` : it opens the file for you and returns a Tz struct:
 //!```text
 //! [dependencies]
 //! libtzfile = { version = "4.0.0", features = ["std"] }
@@ -30,8 +23,6 @@
 //!```text
 //! Tz { tzh_timecnt_data: [-2717643600, -1633273200, -1615132800, -1601823600, -1583683200, -880210800, -820519140, -812653140, -796845540, -84380400, -68659200], tzh_timecnt_indices: [2, 1, 2, 1, 2, 3, 2, 3, 2, 1, 2], tzh_typecnt: [Ttinfo { tt_utoff: -26898, tt_isdst: 0, tt_abbrind: 0 }, Ttinfo { tt_utoff: -21600, tt_isdst: 1, tt_abbrind: 1 }, Ttinfo { tt_utoff: -25200, tt_isdst: 0, tt_abbrind: 2 }, Ttinfo { tt_utoff: -21600, tt_isdst: 1, tt_abbrind: 3 }], tz_abbr: ["LMT", "MDT", "MST", "MWT"] }
 //! ```
-//!
-//! With the ```std``` feature enabled, you can use the question mark for error conversion.
 //!
 //! With the parse or json features enabled, you have access to additional methods.
 //! For instance, to display 2020 DST transitions in France, you can use the transition_times method:
